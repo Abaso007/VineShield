9b4367bbdf97e18e4ff3526222b50ba60291b713
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-08-08 01:01:51 +0300

    gg

--- gui.py
+++ gui.py
@@ -13,6 +13,7 @@ original: https://github.com/Nick-Vinesmoke/VineShield
 
 from tkinter import *
 import customtkinter as gui
+from PIL import ImageTk, Image
 #from ctypes import windll, byref, sizeof, c_int
 #import os
 
@@ -32,11 +33,21 @@ class GUI:
         #self.HWND = windll.user32.GetParent(self.win.winfo_id())
         #barCol = 0x000000FF
         #windll.dwmapi.DwmSetWindowAttribute(self.HWND, 35, byref(c_int(barCol)), sizeof(c_int))
-
+    
+    def Widgets(self):
+        pass
     def Menu(self) -> None:
 
         self.win = gui.CTk()
 
         self.WinConfig()
+        self.Widgets()
+        canvas = gui.CTkCanvas(master=self.win,width=256, height=128, background='white', highlightthickness=0)
+        image = Image.open("logo.png")
+        image = image.resize((256, 128))
+        image = ImageTk.PhotoImage(image)
+        canvas.create_image(128, 64, image=image)
+        canvas.place(relx=0.5, rely=0.18, anchor=CENTER)
+
 
         self.win.mainloop()
